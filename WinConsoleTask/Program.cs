69a1b09721469ad1c8dd510b39d30360dbea4eb4
Maks <belukiubog@gmail.com>
2019-10-24 23:42:04 +0300

    Ok

---
+++ WinConsoleTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinConsoleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, r;
+            Console.Write("Enter X = ");
+            x = Correct();
+            Console.WriteLine("////////////////");
+            Console.Write("Enter Y = ");
+            y = Correct();
+            Console.WriteLine("////////////////");
+            Console.Write("Enter Z = ");
+            z = Correct();
+            Console.WriteLine("////////////////");
+            r = ((2 * Math.Cos(x * x) - (1 / 2)) / ((1 / 2) + Math.Sin(Math.Pow(y, 2 - z)))) + ((z * z) / (7 - (z / 3)));
+            Console.WriteLine($"r = {r}");
+        }
+        public static double Correct()
+        {
+            var a = Console.ReadLine();
+            double x;
+            if(!Double.TryParse(a,out x))
+            {
+                Console.WriteLine("Enter new number!");
+                a = Console.ReadLine();
+                
+            }return x;
+        }
+    }
+}
---
+++ WinFormTask/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y, z, r;
+            if (!double.TryParse(textBox1.Text, out x))
+            {
+                MessageBox.Show("Wrong value X ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!double.TryParse(textBox2.Text, out y))
+            {
+                MessageBox.Show("Wrong value Y ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!double.TryParse(textBox3.Text, out z))
+            {
+                MessageBox.Show("Wrong value Z ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            r = ((2 * Math.Cos(x * x) - (1 / 2)) / ((1 / 2) + Math.Sin(Math.Pow(y, 2 - z)))) + ((z * z) / (7 - (z / 3)));
+            textBox4.Text = r.ToString("F2");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
